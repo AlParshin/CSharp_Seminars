a23cf81fcf77808cd46837572366c6509256d9d4
Alexey Parshin <jax77@mail.ru>
2022-10-16 13:26:03 +0300

    Выполнил домашнее задание 4

---
+++ CSharp_Seminar1/HomeTask4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (n2 >= n1)
+{
+    max = n2;
+}
+else
+{
+    max = n1;
+}
+if (n3 >= max)
+{
+    max = n3;
+}
+Console.WriteLine($"Максимальное число из трех равно {max}");
