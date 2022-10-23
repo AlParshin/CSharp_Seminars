63d04cc18afaf74cc1db5b0dd6c28c0bb48bb265
Alexey Parshin <jax77@mail.ru>
2022-10-23 23:22:59 +0300

    Выполнил домашнее задание 23 (семинар 3)

---
+++ CSharp_Seminar3/HomeTask23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу кубов чисел от 1 до N.
+
+void PrintCube(int N)
+{
+    Console.WriteLine($"Кубы чисел от 1 до {N}:");
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
+Console.WriteLine("Введите N (>1):");
+PrintCube(Convert.ToInt32(Console.ReadLine()));
