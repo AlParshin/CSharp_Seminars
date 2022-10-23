77dc3aa118797e5db71f4631a604143df551cc3f
Alexey Parshin <jax77@mail.ru>
2022-10-23 13:46:17 +0300

    Выполнил домашнее задание 13 (семинар 2)

---
+++ CSharp_Seminar2/HomeTask13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры в числе нет.
+
+long number = new Random().NextInt64(0, 9223372036854775807);
+
+Console.WriteLine($"Наше случайное число = {number}");
+
+if (number < 100)
+{
+    Console.WriteLine($"В нашем случайном числе {number} нет третьй цифры");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine($"Третья цифра в нашем числе = {number}");
+}
