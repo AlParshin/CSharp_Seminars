104c098f800fa2f6340cabbc70452511321b0bf6
Alexey Parshin <jax77@mail.ru>
2022-10-23 21:57:51 +0300

    Выполнил домашнее задание 19 (семинар 3)

---
+++ CSharp_Seminar3/HomeTask19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и определяет, является ли оно палиндромом.
+
+bool Palindrom(int chislo)
+{
+    int[] array = new int[5];
+    int i = 4;
+    while (chislo != 0)
+    {
+        array[i] = chislo % 10;
+        chislo = chislo / 10;
+        i = i - 1;
+    }
+    if ((array[0] == array[4]) && (array[1] == array[3]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int chislo = new Random().Next(10000, 100000);
+bool res;
+
+res = Palindrom(chislo);
+if (res == true)
+{
+    Console.WriteLine($"Наше число {chislo} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Наше число {chislo} не является палиндромом");
+}
