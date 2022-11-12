50432ee6c1af2881c1c6e04bec19d6fabd9002d0
Alexey Parshin <jax77@mail.ru>
2022-11-12 19:57:58 +0300

    Выполнил домашнее задание 25 (семинар 4)

---
+++ CSharp_Seminar4/HomeTask25/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает 2 числа (A и B) и возводит число A в натуральную степень B.
+
+double Stepen(int A, int B)
+{
+    if (B == 0)
+    {
+        return (1);
+    }
+    else if (B < 0)
+    {
+        double res = A;
+        for (int i = 1; i < Math.Abs(B); i++)
+        {
+            res = A * res;
+        }
+        return (1 / res);
+    }
+    else
+    {
+        int res = A;
+        for (int i = 1; i < B; i++)
+        {
+            res = A * res;
+        }
+        return (res);
+    }
+}
+
+Console.WriteLine("Введите число А, которое будем возводить в степень:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Теперь введите степень, в которую будем возводить наше число {A}:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double result = Stepen(A, B);
+Console.WriteLine($"Результат возведения числа {A} в степень {B} равен {result}");
