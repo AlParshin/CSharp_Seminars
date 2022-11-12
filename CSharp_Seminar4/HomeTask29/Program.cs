4e000ea9953cd3515389e8222af1cd44fbffe42b
Alexey Parshin <jax77@mail.ru>
2022-11-12 20:57:09 +0300

    Выполнил домашнее задание 29 (семинар 4)

---
+++ CSharp_Seminar4/HomeTask29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Заполнение массива производит пользователь.
+
+int[] array = new int[8];
+Console.WriteLine("Ввведите 8 элементов массива:");
+for (int i = 0; i < 8; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("А теперь давайте посмотрим на введенные нами элементы:");
+Console.Write("[");
+for (int i = 0; i < 8; i++)
+{
+    if (i == 0)
+    {
+        Console.Write(array[i]);
+    }
+    else
+    {
+        Console.Write($", {array[i]}");
+    }
+}
+Console.Write("]");
