4f3c12ea6dfdfae4fe65f9f897416c05f8d5b223
Alexey Parshin <jax77@mail.ru>
2022-11-23 21:40:31 +0300

    Выполнил домашнее задание 60 (семинар 8)

---
+++ CSharp_Seminar8/HomeTask60/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+
+int[,,] Zapolnenie(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = Convert.ToInt32(new Random().Next(10, 100));
+            }
+        }
+    }
+    return array;
+}
+
+void Vyivod(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"Элемент {array[i, j, k]} расположен на позиции: ({i}, {j}, {k})");
+            }
+        }
+    }
+}
+
+int[,,] array = new int[2, 2, 2];
+array = Zapolnenie(array);
+Console.WriteLine("Наш заполненный массив:");
+Vyivod(array);
