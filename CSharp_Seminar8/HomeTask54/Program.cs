47c35ddf47bac0e3234cd2189b9646269cc92ae1
Alexey Parshin <jax77@mail.ru>
2022-11-23 17:08:27 +0300

    Выполнил домашнее задание 54 (семинар 8)

---
+++ CSharp_Seminar8/HomeTask54/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] Zapolnenie(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(1, 100));
+        }
+    }
+    return array;
+}
+
+void Vyivod(int[,] array)
+{
+    Console.WriteLine();
+    Console.WriteLine("Вот такой симпатичный массивчик у нас получился:");
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "     ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int[,] Sortirovka(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int temp;
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = new int[10, 10];
+
+array = Zapolnenie(array);
+Vyivod(array);
+array = Sortirovka(array);
+Vyivod(array);
