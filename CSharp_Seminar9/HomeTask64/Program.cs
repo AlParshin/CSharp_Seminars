6254b6015d234e73ccb760d26f98d26616a59e9e
Alexey Parshin <jax77@mail.ru>
2022-11-25 19:26:08 +0300

    Выполнил домашнее задание 64 (семинар 9)

---
+++ CSharp_Seminar9/HomeTask64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void Rekursiya(int N)
+{
+    Console.Write(N + " ");
+    if (N == 1) return;
+    Rekursiya(N - 1);
+}
+
+Console.WriteLine("Введите натуральное число N, чтобы оно было > 1 :");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Выведем все натуральные числа от {N} до 1:");
+Rekursiya(N);
